664b789f2717ac0c28a629c427053a0694c77572
Amarjeet Kumar <amarjeetk2612@gmail.com>
2026-03-06 23:33:32 +0530

    Add popup functionality in script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+let popup = document.getElementById("popup");
+
+document.querySelector(".card button").onclick = function(){
+popup.style.display = "block";
+}
+
+document.getElementById("close").onclick = function(){
+popup.style.display = "none";
+}
